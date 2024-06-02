47fda0f53680e57de7fd7f4158933cdfdae41ae5
niz.ag <nishchalag0307@gmail.com>
2024-06-02 06:41:51 +0530

    a_002

--- Website_Checker/Website_Checker.py
+++ Website_Checker/Website_Checker.py
@@ -1,27 +1,50 @@
 import requests
-from fake_useragent import user_agent
+from fake_useragent import UserAgent
 from http import HTTPStatus
 import csv
+import os
+real_path:str = os.path.dirname(os.path.realpath(__file__))
 
 class Webber:
 
-    def get_websites(csv_path:str) ->list[str]:
+    def get_websites(self,csv_path:str) ->list[str]:
 
         websites :list[str]=[]
-        with open(csv_path) as file:
-            reader = csv.reader
-            print(type(reader))
+        with open(f'{real_path}/{csv_path}', 'r') as file:
+            reader = csv.reader(file)
+
             for row in reader:
-                print(row)
                 if "https://" not in row[1]:
                     websites.append(f"https://{row[1]}")
                 else:
                     websites.append(row[1])
         return websites
     
-    def get_user_agent() ->str:
-        ua = user_agent
+    def get_user_agent(self) ->str:
+        ua = UserAgent()
         return ua.chrome
     
-    def get_user_description(status_code:str)->str:
-        description:str = 
+    def get_user_description(self,status_code:int)->str:
+        for value in HTTPStatus:
+            if value == status_code:
+                description: str = f'({value} {value.name}) {value.description}'
+                return description
+        return '??? NOT FOUND'
+
+    def check_website(self,website:str,user_agent):
+        try:
+            code:int = requests.get(website,headers={"User-Agent":user_agent}).status_code
+            print(self.get_user_description(code),website)
+        except Exception as e:
+            print(f"Could not find any information {website}")
+
+    def main(self):
+        sites: list[str] = self.get_websites('websites.csv')
+        user_agent:str=self.get_user_agent()
+
+        for site in sites:
+            self.check_website(site,user_agent)
+
+user = Webber()            
+if __name__ == '__main__':
+    user.main()
